4bf32b00305d2af9fe20d31e37aac282df4399fd
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-04-18 16:41:34 -0300

    Criação da classe main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        Caminhao c1 = null;
+        CarroPasseio cp1 = null;
 
-
+       try {
+           c1 = new Caminhao(2012, "Volvo", "abc-123", 430000.00,
+                   800.00, 100000.00);
+           System.out.println("Veículo: " + c1.getMarca() + "\nIPVA: " + c1.calcularIpva());
+           System.out.println("Valor aluguel: " + c1.alugarVeiculo(200000.00, 20) + "\n");
+       }catch (IllegalArgumentException e){
+           System.out.println(e.getMessage());
+       }
+       try {
+           cp1 = new CarroPasseio(2020, "Ford", "abc-123", 120000.00,
+                   250.00);
+           System.out.println("Veículo: " + cp1.getMarca() + "\nIPVA: " + cp1.calcularIpva());
+           System.out.println("Valor aluguel: " + cp1.alugarVeiculo(0.0, 10) + "\n");
+       }catch (IllegalArgumentException e){
+           System.out.println(e.getMessage());
+       }
     }
 }
